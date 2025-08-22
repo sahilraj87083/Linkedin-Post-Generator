a4ffd266762bd12fa79729590c8db9e0850d6a0c
sahilraj87083 <sahilraj87093@gmail.com>
2025-08-22 19:37:08 +0530

    feat: add basic Streamlit UI with title, length, and language options

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import streamlit as st
 from few_shot import  FewShotPosts
 
+length_options = ["Short" , "Medium", "Long"]
+language_options = ["English" , "Hinglish"]
+
 
 def main():
     st.title("LinkedIn Post Generator")
     col1 , col2, col3 = st.columns(3)
+    fs = FewShotPosts()
 
     with col1:
-        st.selectbox("Title", options=[])
+        st.selectbox("Title", options = fs.get_tags())
+
+    with col2:
+        st.selectbox("Length" , options=length_options)
+
+    with col3:
+        st.selectbox("Language" , options=language_options)
 if __name__ == "__main__":
     main()
