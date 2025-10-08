ba7869d0e965e0fe6d18651c7e8bdeb8baf0d0da
sahilraj87083 <sahilraj87093@gmail.com>
2025-10-08 19:40:37 +0530

    Ready for Render deployment

--- llm_helper.py
+++ llm_helper.py
@@ -10,4 +10,5 @@ llm = ChatGroq(groq_api_key =  os.getenv("GROQ_API_KEY"), model_name = "deepseek
 
 if __name__ == "__main__":
     response = llm.invoke("What are the two main ingredients of samosa")
+    
     print(response.content)
